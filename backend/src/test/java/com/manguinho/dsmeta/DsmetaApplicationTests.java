7ea84a1d29aa93ed60dd06ce55811078a50a7072
brunomanguinho <brunomanguinho@gmail.com>
2022-09-08 20:11:53 -0300

    First commit

---
+++ backend/src/test/java/com/manguinho/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manguinho.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
