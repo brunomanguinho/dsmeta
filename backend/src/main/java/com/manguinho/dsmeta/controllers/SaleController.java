abf8fc727d210085eb357c3e7e2869864e987fa1
brunomanguinho <brunomanguinho@gmail.com>
2022-09-10 13:29:29 -0300

    Date select

--- backend/src/main/java/com/manguinho/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/manguinho/dsmeta/controllers/SaleController.java
@@ -1,24 +1,33 @@
 package com.manguinho.dsmeta.controllers;
 
-import java.util.List;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.manguinho.dsmeta.entities.Sale;
 import com.manguinho.dsmeta.services.SaleService;
 
 @RestController
-@RequestMapping(value="/sales")
+@RequestMapping(value="/sales") // Specifies the web route 
 public class SaleController {
 	
 	@Autowired
 	private SaleService service;
 	
-	@GetMapping 
-	public List<Sale> findSales(){
-		return service.findSales();
+	@GetMapping // @GetMapping links this method to a GET request
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+			Pageable pageable){
+		return service.findSales(minDate, maxDate, pageable);
 	}
+	
+	//@RequestParam specifies that the param came from the request (GET on this case)
+	//and what is the name of the param on that request
 }
